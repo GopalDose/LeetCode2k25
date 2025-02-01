95f08a9196d2c39cd4bf9781cd92b4f3d1258ee5
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-02-01 23:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (2.79%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); ++i)
-            if (!((nums[i] ^ nums[i-1]) & 1)) // XOR reveals parity match
-                return false; // \U0001f6ab Same parity → invalid
-        return true; // ✅ All pairs have opposite parity
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
+                return false;
+            }
+        }
+        return true;
     }
 };
