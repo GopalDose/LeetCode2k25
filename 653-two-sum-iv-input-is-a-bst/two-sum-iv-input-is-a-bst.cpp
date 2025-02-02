a9ce40c118cfaff5b6485585376fa46bd8c22c0d
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-02-02 20:02:29 +0530

    Time: 3 ms (51.79%) | Memory: 37.5 MB (34.98%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,43 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
         vector<int> ans;
-        queue<TreeNode*> q;
-        q.push(root);
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
 
-        while(!q.empty()){
-            int size = q.size();
-
-            for(int i = 0; i<size;i++){
-                TreeNode* t = q.front();
-                q.pop();
-                ans.push_back(t->val);
-
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
+        // Inorder traversal using stack (iterative approach)
+        while (curr || !st.empty()) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
             }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            curr = curr->right;
         }
 
-        int start = 0;
-        int end = ans.size()-1;
-        sort(ans.begin(), ans.end());
-        while(start < end){
-            if(ans[start]+ans[end] == k && start != end) return true;
-            if(ans[start]+ans[end] > k) end--;
-            else{
-                start++;
-            }
+        // Two-pointer approach on sorted 'ans' array
+        int start = 0, end = ans.size() - 1;
+        while (start < end) {
+            int sum = ans[start] + ans[end];
+            if (sum == k) return true;
+            if (sum > k) end--;
+            else start++;
         }
 
         return false;
