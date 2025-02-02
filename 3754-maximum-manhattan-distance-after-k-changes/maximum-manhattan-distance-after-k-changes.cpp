fb4704654260506237111bbbd1bb47835ca0e405
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-02-02 12:58:25 +0530

    Time: 45 ms (81.82%) | Memory: 38.1 MB (18.18%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxDistance(string s, int k) {
-        int ans = 0;
-        int north = 0, south = 0, east = 0, west = 0;
+        int maxDist = 0;
+        int northMoves = 0, southMoves = 0, eastMoves = 0, westMoves = 0;
 
-        for (char c : s) {
-            if (c == 'N') north++;
-            else if (c == 'S') south++;
-            else if (c == 'E') east++;
-            else west++;
+        for (char move : s) {
+            if (move == 'N') northMoves++;
+            else if (move == 'S') southMoves++;
+            else if (move == 'E') eastMoves++;
+            else westMoves++;
 
-            int req = min(north, south) + min(east, west);
-            ans = max(ans, north + south + east + west - max(0, 2 * (req - k)));
+            int oppositeCancels = min(northMoves, southMoves) + min(eastMoves, westMoves);
+            maxDist = max(maxDist, northMoves + southMoves + eastMoves + westMoves - max(0, 2 * (oppositeCancels - k)));
         }
         
-        return ans;
+        return maxDist;
     }
 };
